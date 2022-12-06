ca528884401a8f9d4a93a7f82540e6a5b78da698
Courtney Lippman <courtney.lippman@colorado.edu>
2022-12-06 16:23:03 -0700

    Add display styles to movie details.

--- pomodori/src/MovieInfo.js
+++ pomodori/src/MovieInfo.js
@@ -7,20 +7,19 @@ const MovieInfo = (props) => {
         <section className="movie-info">
             <section className="all-movie-info">
                 <section className="imgs">
-                    <img style={{display: props.movie.backdrop_path ? 'flex' :  'none'}}
- className="bkg-img" src={props.movie.backdrop_path} alt={props.movie.title} />
-                    <img style={{display: props.movie.poster_path ? 'flex' :  'none'}} className="cover-img" src={props.movie.poster_path} alt={props.movie.title} width="500" height="600" />
+                    <img style={{display: !props.movie.backdrop_path &&  'none'}}className="bkg-img" src={props.movie.backdrop_path} alt={props.movie.title} />
+                    <img style={{display: !props.movie.poster_path &&  'none'}}className="cover-img" src={props.movie.poster_path} alt={props.movie.title} width="500" height="600" />
                 </section>
                     <section className="movie-info-txt">
-                        <h1 style={{display: props.movie.title ? 'flex' :  'none'}} id='movie-title'>{props.movie.title}</h1>
-                        <h2>Release Date: {props.movie.release_date}</h2>
-                        <h3>Budget: {props.movie.budget}</h3>
-                        <h3>Revenue: {props.movie.revenue}</h3>
-                        <h3>Runtime: {props.movie.runtime}</h3>
-                        <h3>Average Rating: {props.movie.average_rating}</h3>
-                        <h3>Genres: {genres}</h3>
-                        <h3>Tagline: {props.movie.tagline}</h3>
-                        <p>Overview:{props.movie.overview}</p>
+                        <h1 style={{display: !props.movie.title &&  'none'}} id='movie-title'>{props.movie.title}</h1>
+                        <h2 style={{display: !props.movie.release_date &&  'none'}}>Release Date: {props.movie.release_date}</h2>
+                        <h3 style={{display: !props.movie.budget &&  'none'}}>Budget: {props.movie.budget}</h3>
+                        <h3 style={{display: !props.movie.revenue &&  'none'}}>Revenue: {props.movie.revenue}</h3>
+                        <h3 style={{display: !props.movie.runtime &&  'none'}}>Runtime: {props.movie.runtime}</h3>
+                        <h3 style={{display: !props.movie.average_rating &&  'none'}}>Average Rating: {props.movie.average_rating}</h3>
+                        <h3 style={{display: !props.movie.genres.length &&  'none'}}>Genres: {genres}</h3>
+                        <h3 style={{display: !props.movie.tagline &&  'none'}}>Tagline: {props.movie.tagline}</h3>
+                        <p style={{display: !props.movie.overview &&  'none'}}>Overview:{props.movie.overview}</p>
                     </section>
             </section>
             <button className="glow-hover" onClick={props.buttonClick}>Home</button>
