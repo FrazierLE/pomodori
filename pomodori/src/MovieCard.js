8bb85fc5d1723dc239b81b785b52eb085cff5ad2
Courtney Lippman <courtney.lippman@colorado.edu>
2022-12-10 13:31:12 -0700

    Change navlink to link.

--- pomodori/src/MovieCard.js
+++ pomodori/src/MovieCard.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './MovieCard.css'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function MovieCard({id, poster, title}) {
   return(
     <div>
       <ul>
         <li className='card'>
-          <NavLink to={`/movies/${id}`}>
+          <Link to={`/movies/${id}`}>
             <img src={poster} alt={title} width="500" height="600" />
-          </NavLink>
+          </Link>
         </li>
       </ul>
     </div>
